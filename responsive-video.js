d191e75df18044c0c07621ddc307b0a7d5145ffc
Tina <tinacious.design@gmail.com>
2013-01-11 16:29:45 -0500

    done

--- responsive-video.js
+++ responsive-video.js
@@ -1,27 +1,42 @@
 /*
-* jQuery Responsive Video plugin
-*/
-// Wrap an anonymous function to avoid conflict
+ *  Tinacious Responsive Video jQuery plugin
+ *  Plugin URL: http://github.com/tinacious/responsive-video
+ *
+ *  Authored by Christina Holly (Tinacious Design)
+ *  http://tinaciousdesign.com
+ *
+ *  Copyright 2013, Christina Holly
+ *  License: GNU General Public License, version 3 (GPL-3.0)
+ *  http://www.opensource.org/licenses/gpl-3.0.html
+ *
+ */
+
 (function($){
-	// Attach a method
 	$.fn.extend({
 		responsivetd: function(options) {
-			// Options
 			var defaults = {
+				// Your Video's ID
 				id: '56733081',
+				// Video Type
+				// Options: 'vimeo' (default), 'youtube'
 				type: 'vimeo',
+				// Aspect Ratio
+				// Options: 'widescreen' (default), 'standard'
 				ratio: 'widescreen'
 			}
 			var options = $.extend(defaults, options);
-			// Iterate over the current set of matched elements
 			return this.each(function(){
-				// code here
 				var obj = $(this);
-				if defaults.type == 'vimeo' {
-					obj.append('vimeo');
+				var vimeoCode = '<iframe src="http://player.vimeo.com/video/' + defaults.id + '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+				var youtubeCode = '<iframe src="http://www.youtube.com/embed/' + defaults.id + '" frameborder="0" allowfullscreen></iframe>';
+				if(defaults.type == 'vimeo') {
+					var videoCode = vimeoCode;
+				} else if(defaults.type == 'youtube') {
+					var videoCode = youtubeCode;
 				}
+				var ratio = (defaults.ratio == 'standard') ? ' standardDef' : '';
+				obj.append('<div class="responsive-video' + ratio + '">' + videoCode + '</div>');
 			});
 		}
 	});
-	// pass jQuery to the function so that it can use any Javascript variable name to replace $
 })(jQuery);
